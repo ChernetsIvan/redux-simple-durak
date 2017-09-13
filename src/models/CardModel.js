bb65a6425d5b4286419c2f9bb205c92ee6e360f1
IvanyCH008 <chernets.ivan@gmail.com>
2017-09-13 14:57:18 +0300

    Prettier installed. In the beginning of so cool procedure as Refactoring existing codebase :)

--- src/models/CardModel.js
+++ src/models/CardModel.js
@@ -1,10 +1,10 @@
-class CardModel{
-    constructor(id, rank, suit, power){
-        this.id = id;
-        this.rank = rank;
-        this.suit = suit;
-        this.power = power;
-    }
+class CardModel {
+  constructor(id, rank, suit, power) {
+    this.id = id;
+    this.rank = rank;
+    this.suit = suit;
+    this.power = power;
+  }
 }
 
 export default CardModel;
