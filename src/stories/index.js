3c559a1de01f79fef2085db1b7edcf0dbdb9f6a6
IvanyCH008 <chernets.ivan@gmail.com>
2017-09-21 16:17:22 +0300

    Trying to perform show case for Card component.

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,6 +6,11 @@ import { linkTo } from "@storybook/addon-links";
 
 import { Button, Welcome } from "@storybook/react/demo";
 
+import "./../../public/libs/bootstrap/css/bootstrap.min.css";
+
+import Card from "./../components/GameScreen/Card/Card";
+import "./../components/GameScreen/Card/Card.css";
+
 storiesOf("Welcome", module).add("to Storybook", () => (
   <Welcome showApp={linkTo("Button")} />
 ));
@@ -16,4 +21,24 @@ storiesOf("Button", module)
   ))
   .add("with some emoji", () => (
     <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf("Card", module)
+  .addDecorator(story => (
+    <div class="container">
+      <div class="row">{story()}</div>
+    </div>
+  ))
+  .add("Card with FALSE hidden flag", () => (
+    <Card
+      id={1}
+      rank={new Object({ text: "6", cardValue: 1 })}
+      suit={new Object({ suit: "K" })}
+      onCardClick={action("Card was clicked!")}
+      bootStrapColClass={"col-1"}
+      hidden={false}
+    />
+  ))
+  .add("Card with TRUE hidden flag", () => (
+    <Card key={2} bootStrapColClass="col-1" hidden={true} />
   ));
