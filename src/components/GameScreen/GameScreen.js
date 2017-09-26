3642d404d7ef94377ef36f239fc957b2c2e51f93
IvanyCH008 <chernets.ivan@gmail.com>
2017-09-26 10:59:03 +0300

    GameScreen component from class into function has been rewrited

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 //Компонент-представление
@@ -10,43 +10,41 @@ import AIContainer from "./../../containers/GameScreen/AIContainer";
 import DeckContainer from "./../../containers/GameScreen/DeckContainer";
 import PlayerContainer from "./../../containers/GameScreen/Player/PlayerContainer";
 
-class GameScreen extends Component {
-  render() {
-    return (
-      <div>
-        <div className="container  my-1">
-          <div className="row justify-content-between">
-            <div className="col-auto">
-              <button
-                className="btn btn-secondary mr-3"
-                onClick={this.props.onBeginGameClick}
-              >
-                Новая игра
-              </button>
-            </div>
-            <StatusContainer />
-            <div className="col-2" />
+export const GameScreen = ({ onBeginGameClick, aiField, playerField }) => {
+  return (
+    <div>
+      <div className="container  my-1">
+        <div className="row justify-content-between">
+          <div className="col-auto">
+            <button
+              className="btn btn-secondary mr-3"
+              onClick={onBeginGameClick}
+            >
+              Новая игра
+            </button>
           </div>
-        </div>
-        <div>
-          <AIContainer />
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-10">
-                <Field cards={this.props.aiField} />
-                <Field cards={this.props.playerField} />
-              </div>
-              <div className="col-2">
-                <DeckContainer />
-              </div>
-            </div>
-          </div>
-          <PlayerContainer />
+          <StatusContainer />
+          <div className="col-2" />
         </div>
       </div>
-    );
-  }
-}
+      <div>
+        <AIContainer />
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-10">
+              <Field cards={aiField} />
+              <Field cards={playerField} />
+            </div>
+            <div className="col-2">
+              <DeckContainer />
+            </div>
+          </div>
+        </div>
+        <PlayerContainer />
+      </div>
+    </div>
+  );
+};
 
 export default GameScreen;
 
