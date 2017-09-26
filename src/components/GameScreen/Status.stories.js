62446693d51cee8dcf0079c50619759c8ccfd9b5
IvanyCH008 <chernets.ivan@gmail.com>
2017-09-26 19:17:24 +0300

    Status story has been updated with new Status component! :)

--- src/components/GameScreen/Status.stories.js
+++ src/components/GameScreen/Status.stories.js
@@ -4,10 +4,6 @@ import { storiesOf } from "@storybook/react";
 
 import { Status } from "./Status";
 import * as gameModes from "./../../constants/GameModes";
-import DeckUtils from "./../../utils/DeckUtils";
-
-const cards = DeckUtils.fillCards({ suit: "Ч" });
-const emptyFunc = () => {};
 
 storiesOf("Status", module)
   .addDecorator(story => (
@@ -15,73 +11,16 @@ storiesOf("Status", module)
       <div className="row justify-content-center">{story()}</div>
     </div>
   ))
-  .add("player lose", () => (
-    <Status
-      gameMode={gameModes.AIWin}
-      computerCards={cards}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
-  ))
-  .add("player win", () => (
-    <Status
-      gameMode={gameModes.PlayerWin}
-      computerCards={cards}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
-  ))
-  .add("the draw", () => (
-    <Status
-      gameMode={gameModes.TheDraw}
-      computerCards={cards}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
-  ))
+  .add("player lose", () => <Status gameMode={gameModes.AIWin} />)
+  .add("player win", () => <Status gameMode={gameModes.PlayerWin} />)
+  .add("the draw", () => <Status gameMode={gameModes.TheDraw} />)
   .add("player attack, AI defence", () => (
-    <Status
-      gameMode={gameModes.PlayerAttack}
-      computerCards={cards}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
+    <Status gameMode={gameModes.PlayerAttack} />
   ))
   .add("AI attack, player defence", () => (
-    <Status
-      gameMode={gameModes.AiAttack}
-      computerCards={cards}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
+    <Status gameMode={gameModes.AiAttack} />
   ))
   .add("AI taking cards, player discard him another cards", () => (
-    <Status
-      gameMode={gameModes.PlayerDiscard}
-      computerCards={cards}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
+    <Status gameMode={gameModes.PlayerDiscard} />
   ))
-  .add("AI have not cards", () => (
-    <Status
-      gameMode={gameModes.PlayerAttack}
-      computerCards={cards.slice(0, 0)}
-      fullDeck={cards}
-      onPlayerWin={emptyFunc}
-      onAiWin={emptyFunc}
-      onTheDraw={emptyFunc}
-    />
-  ));
+  .add("AI have not cards", () => <Status gameMode={gameModes.NoCardsAI} />);
