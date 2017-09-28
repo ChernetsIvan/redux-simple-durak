b7af6d8107a2550cb779fa29fbbfb014db369c37
IvanyCH008 <chernets.ivan@gmail.com>
2017-09-28 12:32:18 +0300

    RemoveCardsButton has been refactored

--- src/components/GameScreen/Player/Buttons/RemoveCardsButton.js
+++ src/components/GameScreen/Player/Buttons/RemoveCardsButton.js
@@ -1,31 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-class RemoveCardsButton extends React.Component {
-  render() {
-    let button = null;
-    if (
-      this.props.aiField.length === this.props.playerField.length &&
-      this.props.aiField.length !== 0 &&
-      this.props.playerField.length !== 0
-    ) {
-      let dash = <span>&mdash;</span>;
-      button = (
-        <button
-          className="btn btn-info"
-          onClick={this.props.onRemoveCardsClick}
-        >
-          {dash} Отбой!
-        </button>
-      );
-    } else {
-      button = null;
-    }
-    return button;
-  }
-}
-
-export default RemoveCardsButton;
+export const RemoveCardsButton = ({
+  aiField,
+  playerField,
+  onRemoveCardsClick
+}) => {
+  let dash = <span>&mdash;</span>;
+  return aiField.length === playerField.length &&
+  aiField.length !== 0 &&
+  playerField.length !== 0 ? (
+    <button className="btn btn-info" onClick={onRemoveCardsClick}>
+      {dash} Отбой!
+    </button>
+  ) : null;
+};
 
 RemoveCardsButton.propTypes = {
   aiField: PropTypes.array.isRequired,
