9f65477035e96168afeea9c370f660b7e44cbaac
IvanyCH008 <chernets.ivan@gmail.com>
2017-09-28 12:50:13 +0300

    App component has been refactored :)

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,10 @@ import PropTypes from "prop-types";
 import StartScreenContainer from "./../containers/StartScreen/StartScreenContainer";
 import GameScreenContainer from "./../containers/GameScreen/GameScreenContainer";
 
-class App extends Component {
-  render() {
-    let output = null;
-    if (this.props.showStartScreen) {
-      output = <StartScreenContainer />;
-    } else {
-      output = <GameScreenContainer />;
-    }
-    return <div>{output}</div>;
-  }
-}
+export const App = ({ showStartScreen }) => {
+  return showStartScreen ? <StartScreenContainer /> : <GameScreenContainer />;
+};
 
 App.propTypes = {
   showStartScreen: PropTypes.bool.isRequired
 };
-
-export default App;
