b1070199f01f4cf9b897d5eb4a1d1ad6b91e7a68
IvanyCH008 <chernets.ivan@gmail.com>
2017-10-02 10:19:42 +0300

    Attempt to implement all with new eslint rules

--- src/containers/GameStatisticsContainer.js
+++ src/containers/GameStatisticsContainer.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 
-import { GameStatistics } from "./../components/GameStatistics";
+import GameStatistics from "./../components/GameStatistics";
 
 import { clickOnBeginGameButton } from "./../actions";
 
