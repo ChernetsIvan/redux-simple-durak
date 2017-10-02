4f3ad714529da86746dd6e3a328738fe06b68fa3
IvanyCH008 <chernets.ivan@gmail.com>
2017-10-02 17:02:58 +0300

    Refactoring of DeckUtils.js has been completed. #7: looks, like all are working.

--- src/utils/DeckUtils.js
+++ src/utils/DeckUtils.js
@@ -5,49 +5,49 @@ import { cardRanks, cardSuits } from "./../constants/GameConstants";
 import CardModel from "./../models/CardModel";
 
 class DeckUtils {
-  //отсортировать входной массив карт по возрастанию isAsc=true
-  //(убыванию-> isAsc=false)  по параметру 'power'
+  // отсортировать входной массив карт по возрастанию isAsc=true
+  // (убыванию-> isAsc=false)  по параметру 'power'
   static sortInputDeckByPower(inputDeck, isAsc) {
-    inputDeck.sort(function(a, b) {
+    inputDeck.sort((a, b) => {
       if (isAsc) {
         return a.power - b.power;
-      } else {
-        return b.power - a.power;
       }
+      return b.power - a.power;
     });
   }
 
-  //Возвращает наполненную всеми 36-ю картами колоду
+  // Возвращает наполненную всеми 36-ю картами колоду
   static fillCards(trumpSuit) {
-    let fullDeck = [];
-    //Устанавливает power для каждой карты по принципу:
-    //козырь->10*rank.cardValue, НЕкозырь->1*rank.cardValue
+    const fullDeck = [];
+    // Устанавливает power для каждой карты по принципу:
+    // козырь->10*rank.cardValue, НЕкозырь->1*rank.cardValue
     let z = 0;
-    let power, coef;
-    cardSuits.forEach(function(element, index, array) {
+    let power;
+    let coef;
+    cardSuits.forEach((element) => {
       if (element.suit === trumpSuit.suit) {
         coef = 10;
       } else {
         coef = 1;
       }
-      cardRanks.forEach(function(element2, index2, array2) {
+      cardRanks.forEach((element2) => {
         power = coef * element2.cardValue;
         fullDeck.push(new CardModel(z, element2, element, power));
-        z++;
+        z+=1;
       });
     });
     return fullDeck;
   }
 
-  //Возвращает перемешанную колоду
+  // Возвращает перемешанную колоду
   static shuffle(inputDeck) {
-    var newFullDeck = [];
+    const newFullDeck = [];
     while (inputDeck.length > 0) {
-      //Случайный индекс в inputDeck
+      // Случайный индекс в inputDeck
       let randIndex = Math.random() * (inputDeck.length - 1);
       randIndex = Math.round(randIndex);
 
-      let card = inputDeck[randIndex];
+      const card = inputDeck[randIndex];
       newFullDeck.push(card);
 
       inputDeck.splice(randIndex, 1);
@@ -55,27 +55,25 @@ class DeckUtils {
     return newFullDeck;
   }
 
-  //Перемещает первую попавшуюся карту с козырной мастью
-  //в конец колоды
+  // Перемещает первую попавшуюся карту с козырной мастью
+  // в конец колоды
   static moveAnyCardWithTrumpSuitToTailOfFullDeck(trumpSuit, fullDeck) {
-    let card = find(fullDeck, card => {
-      return card.suit.suit === trumpSuit.suit;
-    });
+    const card = find(fullDeck, fCard => fCard.suit.suit === trumpSuit.suit);
     pull(fullDeck, card);
     fullDeck.push(card);
   }
 
-  //взять из НАЧАЛА (!) колоды fullDeck и положить в inputDeck столько карт,
-  //чтобы в inputDeck в итоге было 6 карт
+  // взять из НАЧАЛА (!) колоды fullDeck и положить в inputDeck столько карт,
+  // чтобы в inputDeck в итоге было 6 карт
   static giveUpToSixCards(fullDeck, inputDeck) {
-    //если хотя бы одно из условий не выполняется, в цикл не заходим:
+    // если хотя бы одно из условий не выполняется, в цикл не заходим:
     while (inputDeck.length < 6 && fullDeck.length > 0) {
-      let card = fullDeck.shift();
+      const card = fullDeck.shift();
       inputDeck.push(card);
     }
   }
 
-  //Возвращает козырную масть (не карту!) для будущей игры
+  // Возвращает козырную масть (не карту!) для будущей игры
   static chooseSuitForTrumpCardInFuture() {
     let randIndex = Math.random() * (cardSuits.length - 1);
     randIndex = Math.round(randIndex);
