9a6beabd7074b1bc0f3dbf0428b03c43fa6444b7
IvanyCH008 <chernets.ivan@gmail.com>
2017-10-05 10:38:06 +0300

    Trying to do not resolve unknown problem, but walk around it :)

---
+++ src/tests/Card.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Card from './../components/GameScreen/Card/Card';
+
+test('Card snapshot testing', () => {
+  const emptyFunc = () => {};
+  const component = renderer.create(
+    <Card
+      id={1}
+      rank={{ text: "6", cardValue: 1 }}
+      suit={{ suit: "К" }}
+      onCardClick={emptyFunc}
+      bootStrapColClass={"col-1"}
+    />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
