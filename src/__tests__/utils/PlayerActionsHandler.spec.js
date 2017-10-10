e7557c678c361c252511f18544cbfec75f968065
IvanyCH008 <chernets.ivan@gmail.com>
2017-10-10 17:21:23 +0300

    Work with tests has been completely done!

--- src/__tests__/utils/PlayerActionsHandler.spec.js
+++ src/__tests__/utils/PlayerActionsHandler.spec.js
@@ -1,10 +1,8 @@
 import PlayerActionsHandler from "./../../utils/PlayerActionsHandler";
 import * as gameModes from "./../../constants/GameModes";
-import DeckUtils from "./../../utils/DeckUtils";
 
 describe("PlayerActionsHandler handleClickOnCard method test", () => {
   const trumpSuit = { suit: "Ч" };
-  const cards = DeckUtils.fillCards(trumpSuit);
   const testCards = [
     {
       id: 10,
@@ -45,8 +43,14 @@ describe("PlayerActionsHandler handleClickOnCard method test", () => {
     {
       id: 8,
       rank: { text: "В", cardValue: 6 },
-      suit: { suit: "Ч" },
+      suit: { suit: "Б" },
       power: 60
+    },
+    {
+      id:9,
+      rank: { text: "7", cardValue: 2 },
+      suit: { suit: "К" },
+      power: 2
     }
   ];
 
@@ -76,5 +80,149 @@ describe("PlayerActionsHandler handleClickOnCard method test", () => {
       [testCards[1]]
     );
     expect(result).toEqual(gameModes.AiAttack);
+  });
+
+  test("should return recieved gameMode, if playerCards.length>0 && aiCards.length>0, cardId is correct, but incorrect gameMode", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      10,
+      [testCards[0]],
+      gameModes.AiDefence,
+      [],
+      [],
+      trumpSuit,
+      [],
+      [testCards[1]]
+    );
+    expect(result).toEqual(gameModes.AiDefence);
+  });
+
+  test("should return recieved gameMode, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerAttack, but playerField.length>0 and fields do not contain such card", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      10,
+      [testCards[0]],
+      gameModes.PlayerAttack,
+      [testCards[2]],
+      [],
+      trumpSuit,
+      [],
+      [testCards[1]]
+    );
+    expect(result).toEqual(gameModes.PlayerAttack);
+  });
+
+  test("should return PlayerDiscard, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerAttack, playerField.length=0", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      4,
+      [testCards[1]],
+      gameModes.PlayerAttack,
+      [],
+      [],
+      trumpSuit,
+      [],
+      [testCards[0]]
+    );
+    expect(result).toEqual(gameModes.PlayerDiscard);
+  });
+
+  test("should return PlayerDiscard, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerAttack, playerField.length>0 and fields contain such card", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      4,
+      [testCards[1]],
+      gameModes.PlayerAttack,
+      [testCards[7]],
+      [],
+      trumpSuit,
+      [],
+      [testCards[0]]
+    );
+    expect(result).toEqual(gameModes.PlayerDiscard);
+  });
+
+  test("should return PlayerDefence, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerDefence, selectedCard.power < aiField.lastCard.power", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      4,
+      [testCards[1]],
+      gameModes.PlayerDefence,
+      [],
+      [testCards[2]],
+      trumpSuit,
+      [],
+      [testCards[0]]
+    );
+    expect(result).toEqual(gameModes.PlayerDefence);
+  });
+
+  test("should return PlayerDefence, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerDefence, selectedCard.power > aiField.lastCard.power, but not correct suit and not trump suit", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      23,
+      [testCards[2]],
+      gameModes.PlayerDefence,
+      [],
+      [testCards[1]],
+      trumpSuit,
+      [],
+      [testCards[0]]
+    );
+    expect(result).toEqual(gameModes.PlayerDefence);
+  });
+
+  test("should return empty string, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerDefence, selectedCard.power > aiField.lastCard.power, correct suit and not trump suit", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      4,
+      [testCards[1]],
+      gameModes.PlayerDefence,
+      [],
+      [testCards[0]],
+      trumpSuit,
+      [],
+      [testCards[2]]
+    );
+    expect(result).toEqual("");
+  });
+
+  test("should return empty string, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerDefence, selectedCard.power > aiField.lastCard.power, not correct suit but trump suit", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      7,
+      [testCards[5]],
+      gameModes.PlayerDefence,
+      [],
+      [testCards[0]],
+      trumpSuit,
+      [],
+      [testCards[2]]
+    );
+    expect(result).toEqual("");
+  });
+
+  test("should return PlayerDiscard if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerDiscard, and fields do not contain such card", () => {
+    const result = PlayerActionsHandler.handleClickOnCard(
+      4,
+      [testCards[1]],
+      gameModes.PlayerDiscard,
+      [],
+      [testCards[0]],
+      trumpSuit,
+      [],
+      [testCards[2]]
+    );
+    expect(result).toEqual(gameModes.PlayerDiscard);
+  });
+
+  test("should return PlayerDiscard, if playerCards.length>0 && aiCards.length>0, cardId is correct, gameMode=PlayerDiscard, and fields contain such card", () => {    
+    const playerCards = [testCards[1]];
+    const playerField = [];
+    const result = PlayerActionsHandler.handleClickOnCard(
+      4,
+      playerCards,
+      gameModes.PlayerDiscard,
+      playerField,
+      [testCards[7]],
+      trumpSuit,
+      [],
+      [testCards[2]]
+    );
+    expect(result).toEqual(gameModes.PlayerDiscard);
+    expect(playerField.length).toEqual(1);
+    expect(playerCards.length).toEqual(0);
   });
 });
